8b2d0f24dafa1ea0cad8e15d6310630fbc589a5b
danchu0214 <naeun5794@gmail.com>
2024-06-18 15:33:45 +0900

    240618 간단수정

--- index.js
+++ index.js
@@ -11,6 +11,24 @@ $(document).ready(function() {
   });
 });
 
+// $(document).ready(function() {
+//   $(window).scroll(function() {
+//     // 현재 스크롤 위치를 가져옵니다.
+//     var scrollPosition = $(window).scrollTop();
+
+//     // 특정 위치 이상으로 스크롤이 내려갔을 때
+//     if (scrollPosition > 1500) { // 여기서 200은 예시입니다. 원하는 위치로 값을 조정하세요.
+//       $('header').css('backdrop-filter', 'blur(3px)');
+//     } else {
+//       $('header').css('backdrop-filter', 'none');
+//     }
+//   });
+// });
+
+
+
+
+
   // 스크롤이 내려가면 헤더가 사라지고, 스크롤이 올라가면 헤더가 내려옴
     // addEventListener('scroll', () => {})
 
@@ -30,7 +48,7 @@ $(document).ready(function() {
       } else {// 스크롤을 내리면 헤더가 숨김
         // $('header').hide();
         // $('header').slideUp();
-        $('header').css({ top: -80 });
+        $('header').css({ top: -100 });
       }
 
       // prev에 현재 스크롤 값 재할당
@@ -71,26 +89,29 @@ window.addEventListener('scroll', function () {
     if (bounds.top < window.innerHeight && bounds.bottom >= 0) {
       // 섹션이 뷰포트 내에 있을 때
       var scrolled = window.pageYOffset - section.offsetTop; // 섹션의 시작점에서 스크롤된 거리를 계산
-      background.style.transform = `translateY(${scrolled * 0.7}px)`; // 제목을 스크롤 속도의 80%(빠르게)로 이동
+      background.style.transform = `translateY(${scrolled * 0.7}px)`;
     }
   });
 });
 
-// window.addEventListener('scroll', function () {
-//   // 스크롤 이벤트 리스너 등록
-//   const sections = document.querySelectorAll('.category'); // 모든 섹션을 가져옴
-
-//   sections.forEach(function (section) {
-//     // 각 섹션에 대해 반복
-//     let bounds = section.getBoundingClientRect(); // 섹션의 위치와 크기 정보를 가져옴
-//     const background = section.querySelector('.category'); // 배경 요소
-
-//     if (bounds.top < window.innerHeight && bounds.bottom >= 0) {
-//       // 섹션이 뷰포트 내에 있을 때
-//       var scrolled = window.pageYOffset - section.offsetTop; // 섹션의 시작점에서 스크롤된 거리를 계산
-//       background.style.transform = `translateY(${scrolled * 0.8}px)`; // 제목을 스크롤 속도의 80%(빠르게)로 이동
-//     }
-//   });
+// const timeline03 = gsap.timeline({
+//   scrollTrigger: {
+//     trigger: '.video',
+//     scroller: '#main_wrap', // neccessary setting for smooth-scrollbar on body
+//     pinType: 'transform', // neccessary setting for smooth-scrollbar on body
+//     start: "-100% top",
+//     end: "0% top",
+//     pin: false,
+//     anticipatePin: 1,
+//     scrub: true,
+//     // markers: true,
+//     invalidateOnRefresh: true // to make it responsive
+//   }
 // });
+// timeline03.to('.parallax .video-bg', {   
+//     y: 0,
+//     ease: "none"
+// });
+
 // aos
 AOS.init();
